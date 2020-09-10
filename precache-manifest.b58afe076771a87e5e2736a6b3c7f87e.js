e4562e825740e41afae7a4cbdc5abd0671118e9e
ismanovN1 <63084460+ismanovN1@users.noreply.github.com>
2020-09-10 07:07:11 +0500

    newsie

---
+++ precache-manifest.b58afe076771a87e5e2736a6b3c7f87e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4b2531fa813a9ddaf4cb206a7633b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd3d44c96f5f155be00",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "83100c432f091cb16ebb",
+    "url": "/static/css/3.aab5f80f.chunk.css"
+  },
+  {
+    "revision": "056482b37001ef873c76",
+    "url": "/static/css/main.e0f81b41.chunk.css"
+  },
+  {
+    "revision": "7dd3d44c96f5f155be00",
+    "url": "/static/js/2.eb3fd1ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.eb3fd1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83100c432f091cb16ebb",
+    "url": "/static/js/3.3f77e2a8.chunk.js"
+  },
+  {
+    "revision": "056482b37001ef873c76",
+    "url": "/static/js/main.cbd5a071.chunk.js"
+  },
+  {
+    "revision": "6cd817397432827ffbd5",
+    "url": "/static/js/runtime-main.b8240904.js"
+  },
+  {
+    "revision": "800765d1082f3732845885eff7274a87",
+    "url": "/static/media/TELEGRAM.800765d1.jpg"
+  },
+  {
+    "revision": "6332fa735c64fc87e13bedaf0e51468d",
+    "url": "/static/media/instagram.6332fa73.jpg"
+  },
+  {
+    "revision": "a104f47125bd459f963aa29bbb8ffac2",
+    "url": "/static/media/matrix23.a104f471.jpg"
+  },
+  {
+    "revision": "7ae5fa4d96521697de32d5566c0d6fff",
+    "url": "/static/media/matrix24.7ae5fa4d.jpg"
+  },
+  {
+    "revision": "0a86688cfdbe97fcf585ed2d4ca377ab",
+    "url": "/static/media/matrix25.0a86688c.jpg"
+  },
+  {
+    "revision": "09537c9b3e9be8bf109025ff259b6f15",
+    "url": "/static/media/matrix3.09537c9b.jpg"
+  },
+  {
+    "revision": "ffb8b5a94018a8c413e6dbeb32d4f446",
+    "url": "/static/media/myphoto1.ffb8b5a9.jpg"
+  },
+  {
+    "revision": "26de9dfd6f1cf0b7a6d878b9d3174739",
+    "url": "/static/media/myphoto2.26de9dfd.jpg"
+  }
+]);
